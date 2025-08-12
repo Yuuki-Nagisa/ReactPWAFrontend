f271d6b07558e36a861abffd2d3f46a70d36c827
Yuuki-Nagisa <paul.dibyojit2005@gmail.com>
2025-08-12 23:48:44 +0530

    ..

--- src/main.tsx
+++ src/main.tsx
@@ -12,4 +12,11 @@ root.render(
   </React.StrictMode>
 );
 
-registerSW();
+registerSW({
+  onNeedRefresh() {
+    console.log("New content available, please refresh.");
+  },
+  onOfflineReady() {
+    console.log("App ready to work offline.");
+  },
+});
