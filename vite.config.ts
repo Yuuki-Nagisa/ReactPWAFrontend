c3c42de5d0c241ee5d586ba4db64c8805a9add37
Yuuki-Nagisa <paul.dibyojit2005@gmail.com>
2025-08-12 23:40:22 +0530

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,33 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' }),],
+  base: "/ReactPWAFrontend/",
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate', manifest: {
+  "name": "My React PWA",
+  "short_name": "ReactPWA",
+  "description": "A Progressive Web App built with React and Vite.",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff",
+  "icons": [
+    {
+      "src": "favicon.svg",
+      "sizes": "any",
+      "type": "image/svg+xml"
+    },
+    {
+      "src": "icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+} }),],
   build: {
     outDir: 'dist',
     sourcemap: true,
